fd91ec2904a3a5a34f99228e2f8b8eab8482489d
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-08-08 17:25:03 +0000

    minor edit

--- kata2.js
+++ kata2.js
@@ -18,9 +18,9 @@ var conditionalSum = function(values, condition) {
         }
         return sum;
     }
-  };
+};
   
-  console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
-  console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
-  console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
-  console.log(conditionalSum([], "odd"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
